a8548066fa2fd53f91b1c1656406fd5dabd38f2f
Secbone <secbone@gmail.com>
2016-01-31 00:48:53 +0800

    change color

--- dist/js/app.js
+++ dist/js/app.js
@@ -194,7 +194,7 @@
 	                    y: 0,
 	                    width: 38,
 	                    height: 100,
-	                    color: "black",
+	                    color: "rgba(130, 212, 96, 0.4)",
 	                    keyframe: function keyframe() {
 	                        this.y = _cabjs2.default._context.height - this.height;
 	                    }
@@ -215,25 +215,23 @@
 	            });
 	            $("#background").attr("width", $(window).width() + "px");
 	            $("#background").attr("height", $(window).height() + "px");
+	            var _self = this;
+	            _cabjs2.default.preframe(function () {
+	                _self.keyFrame();
+	            });
 	            _cabjs2.default.start("background");
 	        }
 	    }, {
-	        key: "startAnimation",
-	        value: function startAnimation() {
-	            var _self = this;
-	            function keyFrame() {
-	                requestAnimationFrame(keyFrame);
-	                _self.state.analyser.getByteFrequencyData(_self.state.frequencyData);
-	                for (var i = 0; i < _self.state.rect_list.length; i++) {
-	                    _self.state.rect_list[i].height = _self.state.frequencyData[i] * 1.5 + 20;
-	                }
+	        key: "keyFrame",
+	        value: function keyFrame() {
+	            this.state.analyser.getByteFrequencyData(this.state.frequencyData);
+	            for (var i = 0; i < this.state.rect_list.length; i++) {
+	                this.state.rect_list[i].height = this.state.frequencyData[i] * 1.5 + 20;
 	            }
-	            keyFrame();
 	        }
 	    }, {
 	        key: "render",
 	        value: function render() {
-	            this.startAnimation();
 	            var url = "song/encounter.mp3";
 	            //let url = "http://m8.songtaste.com/201601042113/b3f90882f40cf165a9da9068649ee0b7/g/20130827/8/8c/8c7dde6be84c8a5312e57e2a67f640b9.mp3";
 	            return _react2.default.createElement(
@@ -292,7 +290,11 @@
 	        this.ctx.fillStyle = "#fff";
 	        this.ctx.fillRect(0, 0, this._context.width, this._context.height);
 	    },
+	    preframe: function preframe(callback) {
+	        this._pre_frame = callback;
+	    },
 	    runKeyframes: function runKeyframes() {
+	        if (this._pre_frame) this._pre_frame.call(this);
 	        this.clear();
 	        this._components.forEach(function (item) {
 	            item.render();
--- jsx/components/waveform.jsx
+++ jsx/components/waveform.jsx
@@ -22,7 +22,7 @@ class WaveForm extends Component {
                 y: 0,
                 width: 38,
                 height: 100,
-                color: "black",
+                color: "rgba(130, 212, 96, 0.4)",
                 keyframe: function() {
                     this.y = CabJS._context.height - this.height;
                 }
@@ -39,21 +39,19 @@ class WaveForm extends Component {
         });
         $("#background").attr("width", $(window).width()+"px");
         $("#background").attr("height", $(window).height()+"px");
+        let _self = this;
+        CabJS.preframe(function() {
+            _self.keyFrame();
+        });
         CabJS.start("background");
     }
-    startAnimation() {
-        let _self = this;
-        function keyFrame() {
-            requestAnimationFrame(keyFrame);
-            _self.state.analyser.getByteFrequencyData(_self.state.frequencyData);
-            for(let i=0; i<_self.state.rect_list.length; i++){
-                _self.state.rect_list[i].height = _self.state.frequencyData[i]*1.5+20;
-            }
+    keyFrame() {
+        this.state.analyser.getByteFrequencyData(this.state.frequencyData);
+        for(let i=0; i<this.state.rect_list.length; i++){
+            this.state.rect_list[i].height = this.state.frequencyData[i]*1.5+20;
         }
-        keyFrame();
     }
     render() {
-        this.startAnimation();
         let url = "song/encounter.mp3";
         //let url = "http://m8.songtaste.com/201601042113/b3f90882f40cf165a9da9068649ee0b7/g/20130827/8/8c/8c7dde6be84c8a5312e57e2a67f640b9.mp3";
         return (
